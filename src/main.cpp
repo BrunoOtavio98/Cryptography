5cfeee46b4812dd680185432023dd11588383bf7
Bruno <brunootavioramos13@gmail.com>
2025-02-23 21:07:44 -0300

    CRYPTO-0001 Agent does not need to specify who it want to listen to

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ int main()
     GenericAgent agent4 = GenericAgent("agent4", channel);
     GenericAgent agent5 = GenericAgent("agent5", channel);
     GenericAgent agent6 = GenericAgent("agent6", channel);
+    GenericAgent agent7 = GenericAgent("agent7", channel);
 
     channel->ListenToAgent(agent1, agent2);
     channel->ListenToAgent(agent3, agent2);
@@ -22,8 +23,15 @@ int main()
     channel->ListenToAgent(agent5, agent2);
     channel->ListenToAgent(agent6, agent2);
 
+    channel->ListenToAllAgents(agent7);
 
     agent2.SendMessage("Hello from agent2\n");
+
+    agent1.SendMessage("Hello from agent1\n");
+    agent3.SendMessage("Hello from agent3\n");
+    agent4.SendMessage("Hello from agent4\n");
+    agent5.SendMessage("Hello from agent5\n");
+    agent6.SendMessage("Hello from agent6\n");
 
     return 0;
 }
